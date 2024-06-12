82e21d77b0baef29f1b4cf3a790b4b0c6c174023
NSILOU BALANDAMIO <nsiloup@gmail.com>
2024-06-12 13:43:44 +0100

    making number list button clickable, and hiding or showing the arrow icons accordingly

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, useState } from "react";
+import { Dispatch, useEffect, useState } from "react";
 import "../styles/Pagination.css";
 import { DataResult } from "../App";
 
@@ -11,24 +11,67 @@ type MyPaginationProps = {
     setCurrentPage: Dispatch<number>
 }
 export default function Pagination({ dataResult, resultsPerPage, setCurrentPage, currentPage}: MyPaginationProps): ReactJsxElm {
-    const [maximumIsReached, setMaximumIsReached] = useState(false);
-    const [minimumIsReached, setMinimumIsReached] = useState(false);
-    let numberOfPages = Math.round(dataResult.totalAccessibleImages / resultsPerPage);
-  
-    numberOfPages === 0 && (numberOfPages = 1); // to have atleast 1 page instead of 0 page;
-    const numbersArr: number[] = [];
-    for(let i=1; i <= numberOfPages; i++) {
-        numbersArr.push(i);
-    }
-    console.log(numbersArr);
+    
+    const leftArrow = document.querySelector<HTMLButtonElement>('.pagination_left');
+    const rightArrow =  document.querySelector<HTMLButtonElement>('.pagination_right');
+    const paginationElm = document.querySelector<HTMLDivElement>(".pagination");
 
-    const pageNumbersBtnList: ReactJsxElm[] = numbersArr.map((number) => {       
+    const numberOfPages = Math.round(dataResult.totalAccessibleImages / resultsPerPage);
+  
+    const staticNumbersArr: number[] = [];
+    for(let i=1; i <= numberOfPages; i++) {
+        staticNumbersArr.push(i);
+    }
+    console.log(staticNumbersArr);
+
+    // Effect for dynamizing the pages arra
+    useEffect(() => {
+        
+        // When the results occupy just one page
+        if(dataResult.totalAccessibleImages <= resultsPerPage) {
+            paginationElm?.classList.add("collapse");
+        };
+
+        // when the current page is the
+        // first element page of the results pages
+        if(currentPage === staticNumbersArr[0]){
+            leftArrow?.classList.add("collapse");
+        };
+
+        // When the current page is the last
+        // element of the results pages
+        if(currentPage === staticNumbersArr[staticNumbersArr.length-1]) {
+            rightArrow?.classList.add("collapse");
+        }
+
+
+        return () => {
+
+            if(dataResult.totalAccessibleImages <= resultsPerPage) {
+                paginationElm?.classList.remove("collapse");
+            }; 
+            if(currentPage === staticNumbersArr[0]){
+                leftArrow?.classList.remove("collapse");
+            };
+            if(currentPage === staticNumbersArr[staticNumbersArr.length-1]) {
+                rightArrow?.classList.remove("collapse");
+            }   
+        }
+
+    }, [dataResult, currentPage, staticNumbersArr]);
+    
+    const pageNumbersBtnList: ReactJsxElm[] = staticNumbersArr.map((number) => {       
         return <li key={number}>
-            <button className="pagination_numbers_btn">
+            <button className="pagination_numbers_btn" onClick={handlePageNumberBtnClick}>
                 {number}
             </button>
         </li>
     });
+
+    function handlePageNumberBtnClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+        const clickedNumberBtn = e.target as HTMLButtonElement;
+        setCurrentPage(Number(clickedNumberBtn.innerHTML));
+    };
     return (
         <div className="pagination">
             <button className="pagination_left">

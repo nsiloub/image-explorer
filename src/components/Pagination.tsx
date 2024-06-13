1744a30961fa52365cb9a26c3ffcd7168e96bc16
NSILOU BALANDAMIO <nsiloup@gmail.com>
2024-06-13 03:32:39 +0100

    built the fully dynamic pagination functionality

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -10,77 +10,102 @@ type MyPaginationProps = {
     resultsPerPage: number,
     setCurrentPage: Dispatch<number>
 }
-export default function Pagination({ dataResult, resultsPerPage, setCurrentPage, currentPage}: MyPaginationProps): ReactJsxElm {
-    
+export default function Pagination({ dataResult, setCurrentPage, currentPage, resultsPerPage}: MyPaginationProps): ReactJsxElm {    
+    const [dynamicPagesNumbersArr, setDynamicPagesNumbersArr] = useState<(string | number)[]>([]);
+
     const leftArrow = document.querySelector<HTMLButtonElement>('.pagination_left');
     const rightArrow =  document.querySelector<HTMLButtonElement>('.pagination_right');
     const paginationElm = document.querySelector<HTMLDivElement>(".pagination");
 
-    const numberOfPages = Math.round(dataResult.totalAccessibleImages / resultsPerPage);
-  
-    const staticNumbersArr: number[] = [];
-    for(let i=1; i <= numberOfPages; i++) {
-        staticNumbersArr.push(i);
-    }
-    console.log(staticNumbersArr);
+    const numberOfRuslts = dataResult.totalAccessibleImages;
+    const numberOfPages = Math.round(numberOfRuslts / resultsPerPage);
+    
 
-    // Effect for dynamizing the pages arra
     useEffect(() => {
         
-        // When the results occupy just one page
-        if(dataResult.totalAccessibleImages <= resultsPerPage) {
+        // When the current page is the first page
+        if(currentPage === 1 ) { 
+            leftArrow?.classList.add("collapse");
+            setDynamicPagesNumbersArr([1, 2, "...", numberOfPages]);         
+        };
+
+        // when the current page is the last page
+        if(currentPage === numberOfPages){
+            rightArrow?.classList.add("collapse");
+            setDynamicPagesNumbersArr([1, "...", numberOfPages-1, numberOfPages]);
+        };
+
+        // when the current page is the second page
+        if(currentPage === 2){
+            setDynamicPagesNumbersArr([...[1, currentPage, currentPage+1, "...", numberOfPages]]);
+        };
+
+        // when the current page is the second last page
+        if(currentPage === numberOfPages-1){
+            setDynamicPagesNumbersArr([1, "...", currentPage-1, currentPage, numberOfPages]);
+        }
+        
+        // when current is not the second, neither the second last
+        if(currentPage > 2 && currentPage < numberOfPages-1){
+            const arr = [1, "...", currentPage - 1, currentPage, currentPage + 1, "...", numberOfPages];
+            setDynamicPagesNumbersArr([...arr]);
+        };
+
+
+        // when there are only 1 page
+        if(numberOfPages === 0 || numberOfPages === 1){
             paginationElm?.classList.add("collapse");
         };
 
-        // when the current page is the
-        // first element page of the results pages
-        if(currentPage === staticNumbersArr[0]){
-            leftArrow?.classList.add("collapse");
-        };
-
-        // When the current page is the last
-        // element of the results pages
-        if(currentPage === staticNumbersArr[staticNumbersArr.length-1]) {
-            rightArrow?.classList.add("collapse");
+        // when the number of pages is only between 2 abd 3
+        if(numberOfPages  > 1 && numberOfPages <= 3) {
+            const arr =  [];
+            for(let i=1; i<numberOfPages; i++){
+                arr.push(i)
+            }
+            setDynamicPagesNumbersArr([...arr]);
         }
-
 
         return () => {
 
-            if(dataResult.totalAccessibleImages <= resultsPerPage) {
-                paginationElm?.classList.remove("collapse");
-            }; 
-            if(currentPage === staticNumbersArr[0]){
-                leftArrow?.classList.remove("collapse");
-            };
-            if(currentPage === staticNumbersArr[staticNumbersArr.length-1]) {
-                rightArrow?.classList.remove("collapse");
-            }   
-        }
-
-    }, [dataResult, currentPage, staticNumbersArr]);
+            setDynamicPagesNumbersArr([]);
+            leftArrow?.classList.remove("collapse");
+            rightArrow?.classList.remove("collapse");
+            paginationElm?.classList.remove("collapse");
+     
     
-    const pageNumbersBtnList: ReactJsxElm[] = staticNumbersArr.map((number) => {       
-        return <li key={number}>
-            <button className="pagination_numbers_btn" onClick={handlePageNumberBtnClick}>
-                {number}
-            </button>
-        </li>
-    });
+    
+    
+        }
+    }, [dataResult, currentPage]);
+    
+    const numbersAndSeparators: ReactJsxElm[] = dynamicPagesNumbersArr.map((elm, ind) => {
+            return <li key={ind}>
+                {
+                    typeof elm == "string" ? 
+                        <div>{elm}</div>: 
+                    typeof elm == "number" ? 
+                        <button className="pagination_numbers_btn" 
+                        onClick={handlePageNumberBtnClick}>{elm}</button>:
+                    null
+                }
+            </li>
+    })
 
     function handlePageNumberBtnClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
         const clickedNumberBtn = e.target as HTMLButtonElement;
         setCurrentPage(Number(clickedNumberBtn.innerHTML));
     };
+
     return (
         <div className="pagination">
-            <button className="pagination_left">
+            <button className="pagination_left" onClick={() => setCurrentPage(currentPage -1)}>
                 <p className="pagination_arrows">{"«"}</p>
             </button>
             <ul className="pagination_numbers">
-                {pageNumbersBtnList}
+                {numbersAndSeparators}
             </ul>
-            <button className="pagination_right">
+            <button className="pagination_right" onClick={() => setCurrentPage(currentPage +1)}>
                 <p className="pagination_arrows">{"»"}</p>
             </button>
         </div>

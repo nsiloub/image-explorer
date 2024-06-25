758fb77f4aa20daa7de60e7a87b82958fb7e42e5
NSILOU BALANDAMIO <nsiloup@gmail.com>
2024-06-25 19:36:51 +0100

    reverted the css :root to the index.css, for global styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import "./styles/variables.css";
-
 import "./assets/fonts/SpaceGrotesk/SpaceGrotesk[wght].ttf";
 import "./assets/fonts/CHASER/CHASER.ttf";
 
